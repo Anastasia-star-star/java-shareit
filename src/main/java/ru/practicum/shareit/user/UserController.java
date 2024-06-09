85652b6b6957c67ae85e0f6f67d5edfd66732e40
Anastasiia <aiarkhipova@yandex.ru>
2024-06-09 21:13:05 +0300

    fix 5

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.markers.Create;
@@ -20,28 +19,24 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public UserDto add(@Validated({Create.class}) @RequestBody UserDto userDto) {
         log.info("Запрос на добавление пользователя {}", userDto);
         return userService.add(userDto);
     }
 
     @GetMapping("/{userId}")
-    @ResponseStatus(HttpStatus.OK)
     public UserDto getById(@PathVariable Long userId) {
         log.info("Запрос на получение пользователя id = {}", userId);
         return userService.getById(userId);
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public List<UserDto> getAll() {
         log.info("Запрос на получение списка всех пользователей");
         return userService.getAll();
     }
 
     @PatchMapping("/{userId}")
-    @ResponseStatus(HttpStatus.OK)
     public UserDto update(@PathVariable Long userId,
                           @Validated({Update.class})
                           @RequestBody UserDto userDto) {
@@ -50,7 +45,6 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}")
-    @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable Long userId) {
         log.info("Запрос на удаление пользователя id = {}", userId);
         userService.delete(userId);
