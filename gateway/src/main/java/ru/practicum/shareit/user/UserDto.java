d47aa8ccefa8b20ab468092c39f56e1f67875204
Anastasiia <aiarkhipova@yandex.ru>
2024-07-22 01:27:15 +0300

    fix: 48

--- gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -9,6 +9,7 @@ import ru.practicum.shareit.user.markers.Update;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -19,9 +20,11 @@ public class UserDto {
     private Long id;
 
     @NotBlank(groups = {Create.class})
+    @Size(max = 100)
     private String name;
 
     @Email(groups = {Create.class, Update.class})
     @NotBlank(groups = {Create.class})
+    @Size(max = 100)
     private String email;
 }
