cd1d063156bbad410efcb98e69b84c8fe5afc946
Anastasiia <aiarkhipova@yandex.ru>
2024-07-22 01:13:43 +0300

    fix: 46

--- gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.CommentDto;
@@ -14,7 +13,7 @@ import javax.validation.constraints.Min;
 
 import static ru.practicum.shareit.Constants.USER_HEADER;
 
-@Controller
+@RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
 @Slf4j
