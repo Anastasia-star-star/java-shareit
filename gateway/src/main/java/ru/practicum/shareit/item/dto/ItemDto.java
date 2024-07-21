8f4269aa2a4abc91f6358e95279e2e922c05eefd
Anastasiia <aiarkhipova@yandex.ru>
2024-07-22 01:24:05 +0300

    fix: 48

--- gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -16,9 +17,11 @@ import javax.validation.constraints.NotNull;
 public class ItemDto {
 
     @NotBlank
+    @Size(max = 255)
     private String name;
 
     @NotBlank
+    @Size(max = 512)
     private String description;
 
     @NotNull
